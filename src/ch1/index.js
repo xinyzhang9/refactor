65d683b74c7a6da92e74c8d7dbc2fda7428c52bd
Xinyang <xinyzhang9@gmail.com>
2019-11-21 23:22:52 +0800

    extract methods && inline variable

--- src/ch1/index.js
+++ src/ch1/index.js
@@ -2,42 +2,57 @@ export const statement = (invoice, plays) => {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-                          { style: "currency", currency: "USD",
-                            minimumFractionDigits: 2 }).format;
   
-    for (let perf of invoice.performances) {
-      const play = plays[perf.playID];
-      let thisAmount = 0;
-  
-      switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-          throw new Error(`unknown type: ${play.type}`);
-      }
-  
+    for (let perf of invoice.performances) {  
       // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+      volumeCredits += volumnCreditFor(perf);
   
       // print line for this order
-      result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-      totalAmount += thisAmount;
+      result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+      totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+
+    function volumnCreditFor(perf) {
+      let volumnCredits = 0;
+      volumnCredits += Math.max(perf.audience - 30, 0);
+      if ("comedy" === playFor(perf).type) volumnCredits += Math.floor(perf.audience / 5);
+      return volumnCredits;
+    }
+
+    function playFor(perf) {
+      return plays[perf.playID];
+    }
+    
+    function amountFor(perf) {
+      let result = 0;
+      switch (playFor(perf).type) {
+        case "tragedy":
+          result = 40000;
+          if (perf.audience > 30) {
+            result += 1000 * (perf.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (perf.audience > 20) {
+            result += 10000 + 500 * (perf.audience - 20);
+          }
+          result += 300 * perf.audience;
+          break;
+        default:
+          throw new Error(`unknown type: ${play.type}`);
+      }
+      return result;
+    }
   }
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+  style: "currency", currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber/100);
+}
+
